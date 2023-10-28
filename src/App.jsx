8acafe233ef5d4f98cc27563561082d59b326a80
suhas.k <121280716+suhas-scrobits@users.noreply.github.com>
2023-10-28 15:46:57 +0530

    feature: create an basic app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import styles from "./app.module.css";
+import GoogleMapReact from "google-map-react";
+import ReactGoogleAutocomplete from "react-google-autocomplete";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [lat, setLat] = useState(20.290276);
+  const [lng, setLng] = useState(79.655479);
+
+  const [position, setPosition] = useState();
+
+  const updatePosition = (map) => {
+    const newPosition = new google.maps.Marker({
+      position: { lat: Number(lat), lng: Number(lng) },
+      map,
+      draggable: true,
+      geodesic: true,
+    });
+
+    google.maps.event.addListener(newPosition, "dragend", () => {
+      const updatedCoordinates = newPosition.getPosition();
+      setLat(updatedCoordinates.lat());
+      setLng(updatedCoordinates.lng());
+    });
+
+    setPosition(newPosition);
+  };
+
+  useEffect(() => {
+    if (position) {
+      position.setOptions({ position: { lat: Number(lat), lng: Number(lng) } });
+    }
+  }, [lat, lng]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className={`${styles.page}`}>
+      <div className={`${styles.leftDiv}`}>
+        <label>latitude</label>
+        <input
+          placeholder="Enter the latitude"
+          type="number"
+          value={lat}
+          onChange={(e) => setLat(+e.target.value)}
+        />
+        <label>longitudes</label>
+        <input
+          placeholder="Enter the longitude"
+          type="number"
+          value={lng}
+          onChange={(e) => setLng(+e.target.value)}
+        />
+
+        <p>or</p>
+        <label>enter your address</label>
+        <ReactGoogleAutocomplete
+          libraries={["places"]}
+          apiKey={import.meta.env.VITE_GOOGLE_KEY}
+          onPlaceSelected={(place) => {
+            setLat(place.geometry.location.lat());
+            setLng(place.geometry.location.lat());
+          }}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className={`${styles.rightDiv}`}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: import.meta.env.VITE_GOOGLE_KEY,
+            libraries: ["places"],
+          }}
+          defaultCenter={{ lat: Number(lat), lng: Number(lng) }}
+          center={{ lat: Number(lat), lng: Number(lng) }}
+          defaultZoom={15}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => updatePosition(map)}
+          // onChange={(e) => console.log(e)}
+          onZoomAnimationEnd={(e) => {
+            position.setOptions({
+              radius: 10000 / (e * 10),
+            });
+          }}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
